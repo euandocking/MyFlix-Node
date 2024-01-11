8412c3ed86ab1dd48a912670470922578ff23424
euandocking <euandocking@hotmail.co.uk>
2024-01-11 03:32:07 +0000

    switched to environment variable for mongo connection

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const mongoURI = process.env.MONGODB_URI
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/netflix-clone', {
+mongoose.connect(mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
